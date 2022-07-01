e43ae71cb4223fd8acd00a3456abb944751a46f3
Lukas Santillán <lukassantillan2001@gmail.com>
2022-07-01 15:25:10 -0300

    cambios en la función de chequeo, ahora es jugable

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 <h1>Recreación del juego de navegador Wordle en python</h1>
 <br>
-<p>Con fines de practicar lo que conozco de python decidí ponerme de ejercicio el desarrollo de un clon de wordle en la terminal, sigue sin estar terminado. El algoritmo para verificar las letras está incompleto, es posible que tenga que hacer uno desde cero para que funcione apropiadamente. Debajo dejo un pequeño To-Do para recordarme lo que tengo que trabajar para mejorar este proyecto.</p>
+<p>Con fines de practicar lo que conozco de python decidí ponerme de ejercicio el desarrollo de un clon de wordle en la terminal, sigue sin estar terminado. El algoritmo para verificar las letras está incompleto, actualmente es funcional pero falta pulirlo. Debajo dejo un pequeño To-Do para recordarme lo que tengo que trabajar para mejorar este proyecto.</p>
 <br>
 <h2>Para hacer</h2>
-<p>- Arreglar problema con la función de chequeo.</p>
-<p>- Añadir verificación para cortar el programa cuando se alcance el acierto completo de la palabra</p>
+<p>- Hacer que el chequeo no marque "letra en otro lugar" en el caso de que la palabra ingresada tenga letras repetidas y una de esas ande en el lugar correcto.</p>
 <p>- Cambiar el output para que sea más ordenado</p>
 <p>- Añadir la capacidad de poder usar la palabra del día o múltiples de una lista ajena</p>
-<p>- Añadir verificación de palabra válida</p>
+<p>- Añadir verificación de input de palabra válida</p>
+<p>- Añadir validación para la cantidad de letras</p>
--- wordle.py
+++ wordle.py
@@ -1,29 +1,34 @@
 #función de chequeo
 
-def chequeo (rx, gm):
+def chequeo (og, word):
     id = 0
-    acertadas = 0
-    for letra in gm:
-        if letra == rx[id]:
-            print(letra, "es correcta")
-            acertadas += 1
-        elif letra != rx[id]:
-            for letras in rx:
-                if letras == letra:
-                    print(letra, "se encuentra en otro lugar")
-        else:
-            print(letra, "no se encuentra")
-        id += 1
-    if acertadas == 5:
-        mensaje = "Ganaste en", intentos, "intentos!"
-        return mensaje
-
+    if word == og:
+        victoria = 1
+        return victoria
+    else:
+        for letra in word:
+            if letra == og[id]:
+                print(letra, "es correcta")
+            elif letra != og[id] and letra in og:
+                for letras in og:
+                    if letras == letra:
+                        print(letra, "se encuentra en otro lugar")
+            else:
+                print(letra, "no se encuentra")
+            id += 1
             
-intentos = 1
+intentos = 0
 #contenedor de palabra, tal vez podría ser una lista de la que buscar palabras para variarlo
-palabra = "comer"
+palabra = "copia"
+victoria = 0
 
 #input
-while intentos < 7:
+while intentos < 7 and victoria != 1:
     inputPalabra = input(str("Palabra de 5 letras: "))
-    chequeo(palabra, inputPalabra)
+    intentos += 1
+    victoria = chequeo(palabra, inputPalabra)
+
+if intentos == 7:
+    print("Perdiste :(")
+else:
+    print("Ganaste en", intentos, "intentos!")
